e649331d786196b0e8d6d79b27ce259eb2cf082b
mingkai <liuMingKai2020@outlook.com>
2020-10-05 11:33:50 +0800

    模仿成功

--- ReversingLinkedList.c
+++ ReversingLinkedList.c
@@ -13,32 +13,33 @@ struct Node{
 }; 
 typedef PtrToNode List;
 struct Node Arr[MaxSize];
-List ReadList(Position firstAddr,int N){
+List ReadList(Position firstAddr,int *length){
 	List p=(List)malloc(sizeof(struct Node));
 	p->Nextptr=NULL;
 	List L=p;
 	int i;
 	int Addr;
-	for(i=0;i<N;i++){
+	for(i=0;i<(*length);i++){
 		scanf("%d",&Addr);
 		Arr[Addr].Address=Addr;
 		scanf("%d%d",&Arr[Addr].Data,&Arr[Addr].Next);
 	}
+	int size=0;
 	int pos=firstAddr;
-	while(p->Next!=-1){
+	while(Arr[pos].Address!=-1){
 		p->Nextptr=&Arr[pos];
 		p=p->Nextptr;
-		if(p->Next==-1) break;
+		++size;
+		if(Arr[pos].Next==-1) break;
 		pos=p->Next;
 	}
+	*length=size;
 	List temp=L;L=L->Nextptr;free(temp);
 	return L;
 }
 List ReverseList(List L,int k){
 	List temp;
-	List head=(List)malloc(sizeof(struct Node));
-	head->Nextptr=L;
-	List newNode=head->Nextptr;
+	List newNode=L->Nextptr;
 	List oldNode=newNode->Nextptr;
 	int cnt=1;
 	while(cnt<k){
@@ -48,8 +49,25 @@ List ReverseList(List L,int k){
 		oldNode=temp;
 		cnt++;
 	}
-	head->Nextptr->Nextptr=oldNode;
-	return newNode;
+	List rear=L->Nextptr;
+	rear->Nextptr=oldNode;
+	L->Nextptr=newNode;
+	return rear;
+}
+List Reverse(List L,int N,int k){
+	if(k==1) return L; 
+	List head=(List)malloc(sizeof(struct Node));
+	head->Nextptr=L;
+	List p=head;
+	int times=N/k;
+	while(times--){
+		p=ReverseList(p,k);
+	}
+	if(N%k==0) p->Nextptr=NULL;
+	List tem=head;
+	head=head->Nextptr;
+	free(tem);
+	return head;
 }
 void PrintList(List L){
 	List p=L;
@@ -62,9 +80,9 @@ void PrintList(List L){
 int main(){
 	int N,k;
 	Position firstAddr;
-	scanf("%d%d%d",&firstAddr,&N,&k);
-	List L=ReadList(firstAddr,N);
-	L=ReverseList(L,k);	
+	scanf("%d%d%d",&firstAddr, &N, &k);
+	List L=ReadList(firstAddr, &N);
+	L=Reverse(L, N, k);
 	PrintList(L);
 	return 0;
 } 
